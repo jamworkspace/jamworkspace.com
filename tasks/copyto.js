eabf537fc4d6c9b6a82b075782cee42d143bb207
Ezekiel Chentnik <ezekiel.chentnik@booking.com>
2014-06-23 21:53:14 +0200

    bower

--- tasks/copyto.js
+++ tasks/copyto.js
@@ -14,7 +14,7 @@ module.exports = function copyto(grunt) {
 	        options: {
 	            ignore: [
 	            	'public/css/**/*',
-	                
+	               'public/components/**/*',
 	                'public/templates/**/*'
 	            ]
 	        }
