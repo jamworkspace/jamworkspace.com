91d885c35d00bc89e342784ac42fc869972a4d51
Ezekiel Chentnik <ezekiel.chentnik@booking.com>
2014-07-06 19:22:47 +0200

    add membership model

--- controllers/index.js
+++ controllers/index.js
@@ -1,42 +1,41 @@
 'use strict';
 
-
 var IndexModel = require('../models/index'),
     ProfileModel = require('../models/profile'),
     AdminModel = require('../models/admin'),
     AboutModel = require('../models/about'),
+    MembershipModel = require('../models/membership'),
     auth = require('../lib/auth');
-
 
 module.exports = function (router) {
 
-    var indexmodel = new IndexModel();
-    var profilemodel = new ProfileModel();
-    var adminmodel = new AdminModel();
-    var aboutmodel = new AboutModel();
-
+    var indexModel = new IndexModel(),
+        profileModel = new ProfileModel(),
+        adminModel = new AdminModel(),
+        aboutModel = new AboutModel(),
+        membershipModel = new MembershipModel();
 
     router.get('/', function (req, res) {
-        res.render('index', indexmodel);
+        res.render('index', indexModel);
     });
 
-
     router.get('/profile', function(req, res) {
-        res.render('profile', profilemodel);
+        res.render('profile', aboutModel);
     });
 
 
     router.get('/admin', auth.isAuthenticated('admin'), auth.injectUser(), function(req, res) {
-        res.render('admin', adminmodel);
+        res.render('admin', adminModel);
     });
 
     router.get('/about', function(req, res) {
-        res.render('about', aboutmodel);
+        res.render('about', aboutModel);
     });
 
-    /**
-     * Allow the users to log out
-     */
+    router.get('/membership', function(req, res) {
+        res.render('membership', membershipModel);
+    });
+
     router.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/login');
---
+++ models/membership.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+module.exports = function MembershipModel() {
+    return {
+        name: 'membership'
+    };
+};
